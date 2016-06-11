651ccfac4cdfad7ff5bbbe8e787b7a7a84c7cc27
jack-rouleau <jack.rouleau.jr@gmail.com>
2016-06-11 12:26:22 -0400

    DEGREE macro

--- D3DApp/D3DUtils.h
+++ D3DApp/D3DUtils.h
@@ -13,11 +13,16 @@
 //int (WINAPIV * __vsnprintf)(char *, size_t, const char*, va_list) = _vsnprintf;
 #define PI 3.14159265358979323846f
 #define DEG_TO_RAD PI / 180.0f
+#define RAD_TO_DEG  180.0f / PI
 #define RADIAN(x)\
 {\
 	 x * DEG_TO_RAD\
 }
 
+#define DEGREE(y)\
+{\
+	 y * RAD_TO_DEG\
+}
 
 #define ReleaseCOM(x)	\
 {						\
